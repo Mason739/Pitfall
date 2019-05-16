6abb58ab86f04da504fcbdaca003d688eacdbc42
Mason739 <>
2019-05-16 12:13:57 -0400

    Add basic files

---
+++ src/js/controls.js
@@ -0,0 +1,157 @@
+/**
+ * Provides control functionality
+ * Allows for easy customization of the controls, and a centralized control
+ *   system
+ * @type {Object}
+ */
+let controls = {
+  /**
+   * Contains the key bindings for all of the controls of the game
+   * @type {Object}
+   */
+  bindings: {
+
+  },
+
+  /**
+   * Stores the currently pressed and not pressed keys.
+   * @type {Object}
+   */
+  current: {
+
+  },
+
+  /**
+   * Stores the current state of the right mouse button.
+   * @type {Boolean}
+   */
+  rightMouseDown: false,
+
+  /**
+   * Stores the current state of the left mouse button.
+   * @type {Boolean}
+   */
+  leftMouseDown: false,
+
+
+  /**
+   * Checks if the given control is currently pressed.
+   * If a nonexistant control is passed, False is returned
+   * @param  {String} control  the name of the control (as a string)
+   * @return {Boolean}         True if the key is pressed, False if it is not
+   */
+  isControlPressed: function( control ) {
+    if (this.bindings.hasOwnProperty(control)) {
+      // List of all the options for keys to press to activate the control
+      const options = this.bindings[control];
+
+      for (let i = 0; i < options.length; i++) {
+        if (controls.current[options[i]]) {
+          return true;
+        }
+      }
+
+      // if a matching control has not been found after looping through the
+      // entire list, return false
+      return false;
+    } else { // if the control does not have a binding, return false
+      return false;
+    }
+  },
+
+  /**
+   * Logs all of the controls and if they are currently pressed or not
+   * (For debugging)
+   * @return {undefined} No return value
+   */
+  logControls: function() {
+    console.log('====CURRENT CONTROLS===========================');
+
+    console.log("Mouse:");
+    console.log("\tR: " + this.rightMouseDown);
+    console.log("\tL: " + this.leftMouseDown);
+
+    Object.keys(controls.current).forEach((key) => {
+      console.log(key.toString() + controls.current[key]);
+      // console.log('    ' + key.toString() + '  ' +
+      //   controls.isControlPressed(key).toString());
+    });
+
+
+    console.log('===============================================');
+  },
+
+  /**
+   * The current x coordinate of the mouse
+   * @type {Number}
+   */
+  mouseX: 0,
+
+  /**
+   * The current y coordinate of the mouse
+   * @type {Number}
+   */
+  mouseY: 0,
+
+  // TODO: Automatically convert these values to world units
+};
+
+// Key presses
+$(document).keydown(function(e){
+    var checkWebkitandIE=(e.which==26 ? 1 : 0);
+    var checkMoz=(e.which==122 && e.ctrlKey ? 1 : 0);
+
+    controls.current[e.which] = true;
+});
+
+$(document).keyup(function(e){
+    var checkWebkitandIE=(e.which==26 ? 1 : 0);
+    var checkMoz=(e.which==122 && e.ctrlKey ? 1 : 0);
+
+    controls.current[e.which] = false;
+});
+
+
+// Mouse clicks
+$(document).mousedown(function(event) {
+  switch (event.which) {
+    // left mouse
+    case 1:
+      controls.leftMouseDown = true;
+      break;
+    // middle mouse
+    case 2:
+      break;
+    // right mouse
+    case 3:
+      controls.rightMouseDown = true;
+      break;
+    default:
+      break;
+    }
+});
+
+$(document).mouseup(function(event) {
+  switch (event.which) {
+    // left mouse
+    case 1:
+      controls.leftMouseDown = false;
+      break;
+    // middle mouse
+    case 2:
+      break;
+    // right mouse
+    case 3:
+      controls.rightMouseDown = false;
+      break;
+    default:
+      break;
+    }
+});
+
+
+// Get mouse movements
+document.addEventListener("mousemove", function(e) {
+  controls.mouseX = e.clientX;
+  controls.mouseY = e.clientY;
+}, false);
---
+++ src/js/main.js
@@ -0,0 +1,139 @@
+let game = {};
+
+function startGame() {
+
+
+  requestAnimationFrame(game.mainLoop);
+
+  /**
+  * The canvas element.
+  * @type {Object}
+  */
+  game.canvas = {
+    element: document.getElementById("myCanvas")
+  }
+
+  game.canvas.ctx = game.canvas.element.getContext("2d");
+
+  game.start();
+
+}
+
+
+/**
+* Define the namespace for all game operations
+* @namespace
+*/
+game = {
+  lastFrameTimeMs: 0,
+  delta: 0,
+
+  started: false,
+  start: function() {
+    this.started = true;
+  },
+
+  /**
+  * True if the game is running. False if not (ie paused)
+  * @type {Boolean}
+  */
+  running: true,
+
+  pause: function() {
+    // BUG: Will delayed function calls mess up the pause? Yes. Better question, are there any important delayed function calls. I forget what they are called
+    if(this.running) {
+      this.running = false;
+      clock.pause();
+    }
+  },
+
+  play: function() {
+    if(!this.running) {
+      this.running = true;
+      clock.play();
+    }
+  },
+
+  togglePlayPause: function() {
+    if(this.running) {
+      this.pause();
+    } else {
+      this.play();
+    }
+  },
+
+
+  /**
+  * The object that contains all of the data about the current level.
+  * @type {Object}
+  */
+  world: {},
+
+  /**
+  * Has the pause key been released since being pressed?
+  * Prevents very fast switching between paused and unpaused
+  * @type {Boolean}
+  */
+  pauseKeyReleased: true,
+
+  update: function(delta) {
+    if(!this.started) {
+      return;
+    }
+    camera.updateSize();
+    camera.updatePosition();
+
+    if(controls.isControlPressed('PAUSE')) {
+      if(this.pauseKeyReleased) {
+        this.pauseKeyReleased = false;
+        this.togglePlayPause();
+      }
+    } else {
+      this.pauseKeyReleased = true;
+    }
+
+    if(this.running) {
+      // Game step code
+    }
+
+  },
+
+  mainLoop: function(timestamp) {
+    // Throttle the frame rate.
+    if (timestamp < game.lastFrameTimeMs + (1000 / MAX_FPS)) {
+      requestAnimationFrame(game.mainLoop);
+      return;
+    }
+    game.delta += timestamp - game.lastFrameTimeMs;
+    game.lastFrameTimeMs = timestamp;
+
+    while (game.delta >= TIMESTEP) {
+      game.update(TIMESTEP);
+      game.delta -= TIMESTEP;
+    }
+    camera.draw();
+    requestAnimationFrame(game.mainLoop);
+  },
+
+
+};
+
+
+
+
+
+/**
+* Stores the maximum number of frames to render per second.
+* @type {Number}
+* @default
+* @constant
+*/
+const MAX_FPS = 60;
+
+/**
+* The time constant for each render step of the world
+* @type {Number}
+* @constant
+* @default
+*/
+const TIMESTEP = 1000 / 60;
